acc2b7fa3cdd4529dc03b138d0f335e03ef2589f
Yiying Gao <88369383+viggieG@users.noreply.github.com>
2023-10-23 18:21:54 -0500

    Update euclidean_distance_and_cosine_similarity.py

--- euclidean_distance_and_cosine_similarity.py
+++ euclidean_distance_and_cosine_similarity.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def euclid_dist(a, b):
+def euclidean_distance(a, b):
 	temp = a - b
 	euclid_dist = np.sqrt(np.dot(temp.T, temp))
 	return euclid_dist
 
-def cos_sim(a, b):
+def cosine_similarity(a, b):
 	dot_product = np.dot(a, b)
 	norm_a = np.linalg.norm(a)
 	norm_b = np.linalg.norm(b)
